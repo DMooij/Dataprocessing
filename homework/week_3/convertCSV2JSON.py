170cfe60bc9e1701bb87417513177e5f1cd03184
DMooij <dewi.mooij@gmail.com>
2018-04-26 12:06:48 +0200

    link

--- homework/week_3/convertCSV2JSON.py
+++ homework/week_3/convertCSV2JSON.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 # Dewi Mooij
 # 10752978
+# Converts a csvfile to a json
 
 import csv
 import json
 
+# open csv file and json file
 csvfile = open('ufo_gb.csv', 'r')
 jsonfile = open('ufogb.json', 'w')
 
+# assign fieldnames
 fieldnames = ("datetime","shape")
-reader = csv.DictReader(csvfile, fieldnames)
-data = json.dumps( [ row for row in reader ] )
-jsonfile.write("{\"data\": " + data + "}")
 
+# read csvfile
+reader = csv.DictReader(csvfile, fieldnames)
+
+# write data to json
+data = json.dumps( [ row for row in reader ] )
+jsonfile.write(data);
+
+# close csvfile and jsonfile
 csvfile.close()
 jsonfile.close()
